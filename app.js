a2ddcea2b266c9dde913ca8f5ddbd7348385e0c0
alex.fan <alex.fan@okchem.com>
2017-10-19 13:18:29 +0800

    1. fix a bug
        by simply adding a \ before the handlers in frontend template to avoid the client-side templates will be parsed by the view engine on the server
    
        more info pls refer to http://tilomitra.com/handlebars-on-the-server-and-client/

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var engines = require('consolidate');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -12,6 +13,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('hbs', engines.handlebars);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  try {
+    res.render('index', { title: 'Express' });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
