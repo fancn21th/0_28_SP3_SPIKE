bf2e293c7ba5ec0871d902822e5f6e7ba025f4ad
alex.fan <alex.fan@okchem.com>
2017-10-19 14:52:38 +0800

    1. demo 1 for template in client is complete

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   try {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'handlebarsjs running on both server and client' });
   } catch (err) {
     next(err);
   }
